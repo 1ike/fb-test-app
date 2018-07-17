1b8250756fad07a3b442ad36396e2f1cc996b387
1ike <googlohren@gmail.com>
2018-07-18 00:18:02 +0300

    Add Travis.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/1040a9ee5544366d2511/maintainability)](https://codeclimate.com/github/1ike/fb-test-app/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/1040a9ee5544366d2511/test_coverage)](https://codeclimate.com/github/1ike/fb-test-app/test_coverage)
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/__fixtures__/initialStore.js
+++ src/__fixtures__/initialStore.js
@@ -1,111 +1,59 @@
+const options = {
+  preset: "islands#blueCircleDotIconWithCaption",
+  draggable: true
+};
+
+const point1 = {
+  id: 0,
+  name: "p0",
+  placemark: {
+    geometry: {
+      coordinates: [55.78, 37.63]
+    },
+    properties: {
+      hintContent: "p0",
+      balloonContent: "p0"
+    },
+    options
+  }
+};
+const point2 = {
+  id: 1,
+  name: "p1",
+  placemark: {
+    geometry: {
+      coordinates: [55.74, 37.62]
+    },
+    properties: {
+      hintContent: "p1",
+      balloonContent: "p1"
+    },
+    options
+  }
+};
+const point3 = {
+  id: 2,
+  name: "p2",
+  placemark: {
+    geometry: {
+      coordinates: [55.76, 37.7]
+    },
+    properties: {
+      hintContent: "p2",
+      balloonContent: "p2"
+    },
+    options
+  }
+};
+
 export default {
-  points: [
-    {
-      id: 0,
-      name: "p0",
-      placemark: {
-        geometry: {
-          coordinates: [55.78, 37.63]
-        },
-        properties: {
-          hintContent: "p0",
-          balloonContent: "p0"
-        },
-        options: {
-          preset: "islands#blueCircleDotIconWithCaption",
-          draggable: true
-        }
-      }
-    },
-    {
-      id: 1,
-      name: "p1",
-      placemark: {
-        geometry: {
-          coordinates: [55.74, 37.62]
-        },
-        properties: {
-          hintContent: "p1",
-          balloonContent: "p1"
-        },
-        options: {
-          preset: "islands#blueCircleDotIconWithCaption",
-          draggable: true
-        }
-      }
-    },
-    {
-      id: 2,
-      name: "p2",
-      placemark: {
-        geometry: {
-          coordinates: [55.76, 37.7]
-        },
-        properties: {
-          hintContent: "p2",
-          balloonContent: "p2"
-        },
-        options: {
-          preset: "islands#blueCircleDotIconWithCaption",
-          draggable: true
-        }
-      }
-    }
-  ]
+  points: [point1, point2, point3]
 };
 
 export const reorderedPoints = {
   points: [
-    {
-      id: 1,
-      name: "p1",
-      placemark: {
-        geometry: {
-          coordinates: [55.74, 37.62]
-        },
-        properties: {
-          hintContent: "p1",
-          balloonContent: "p1"
-        },
-        options: {
-          preset: "islands#blueCircleDotIconWithCaption",
-          draggable: true
-        }
-      }
-    },
-    {
-      id: 0,
-      name: "p0",
-      placemark: {
-        geometry: {
-          coordinates: [55.78, 37.63]
-        },
-        properties: {
-          hintContent: "p0",
-          balloonContent: "p0"
-        },
-        options: {
-          preset: "islands#blueCircleDotIconWithCaption",
-          draggable: true
-        }
-      }
-    },
-    {
-      id: 2,
-      name: "p2",
-      placemark: {
-        geometry: {
-          coordinates: [55.76, 37.7]
-        },
-        properties: {
-          hintContent: "p2",
-          balloonContent: "p2"
-        },
-        options: {
-          preset: "islands#blueCircleDotIconWithCaption",
-          draggable: true
-        }
-      }
-    }
+    JSON.parse(JSON.stringify(point2)),
+    JSON.parse(JSON.stringify(point1)),
+    JSON.parse(JSON.stringify(point3))
   ]
 };
